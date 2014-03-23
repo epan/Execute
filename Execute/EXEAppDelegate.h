82abdfee4b4e913f9edcb630d53dc122a71991d1
Eric Pan <ericpan@gmail.com>
2014-03-22 20:55:19 -0700

    Initial Commit

---
+++ Execute/EXEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EXEAppDelegate.h
+//  Execute
+//
+//  Created by Eric Pan on 3/22/14.
+//  Copyright (c) 2014 Eric Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EXEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Execute/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Execute
+//
+//  Created by Eric Pan on 3/22/14.
+//  Copyright (c) 2014 Eric Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EXEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EXEAppDelegate class]));
+    }
+}
