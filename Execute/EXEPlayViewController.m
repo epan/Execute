fd44faa89f7496a533ead1f8ff2d3071e0584e98
Eric Pan <ericpan@gmail.com>
2014-03-22 22:29:48 -0700

    Adds alert message from input

--- Execute/EXEPlayViewController.m
+++ Execute/EXEPlayViewController.m
@@ -14,11 +14,16 @@
     UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(20.0f, 40.0f, 280.0f, 44.0f)];
     textField.backgroundColor = [UIColor colorWithWhite:0.95f alpha:1.0f];
     textField.delegate = self;
+    textField.returnKeyType = UIReturnKeySend;
     [self.view addSubview:textField];
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    NSLog(@"Text: %@", textField.text);
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Message" message:textField.text delegate:nil cancelButtonTitle:@"Got it" otherButtonTitles:nil];
+    [alert show];
+    [textField resignFirstResponder];
+    textField.text = nil;
+    
     return NO;
 }
 
