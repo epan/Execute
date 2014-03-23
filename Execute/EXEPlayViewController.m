d2b6a26424c9a01aa97bd5e8a5319fb65a687809
Eric Pan <ericpan@gmail.com>
2014-03-22 21:57:27 -0700

    Adds highlight button color

--- Execute/EXEPlayViewController.m
+++ Execute/EXEPlayViewController.m
@@ -13,7 +13,15 @@
 - (void)viewDidLoad {
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20.0f, 40.0f, 280.0f, 44.0f)];
     label.text = @"Hello world";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor = [UIColor redColor];
     [self.view addSubview:label];
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(20.0f, 100.0f, 280.0f, 44.0f)];
+    [button setTitle:@"Press Me" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor colorWithRed:0.0f green:0.0f blue:1.0f alpha:0.5f] forState:UIControlStateHighlighted];
+    [self.view addSubview:button];
 }
 
 @end
